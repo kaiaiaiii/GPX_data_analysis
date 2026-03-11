aa193195366baa057ffd07de67e7281f31275ef8
kaiaiaiii <kaihrig2012@gmail.com>
2026-03-11 10:09:45 +0100

    userinterface started

--- ux/userinterface.py
+++ ux/userinterface.py
@@ -1,3 +1,19 @@
 ##What to do as a user
 ## resolution of heightmap
 ## number of plots
+import tkinter as tk
+
+window = tk.Tk()
+window.title("GPX Data Analysis")
+
+def get_entry_value_resolution():
+    resolution = entry.get()
+    return resolution
+
+entry = tk.Entry(window)
+entry.pack()
+
+button = tk.Button(window, text="Get Entry Value", command=get_entry_value)
+button.pack()
+
+window.mainloop()
